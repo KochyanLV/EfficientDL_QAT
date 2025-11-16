1866127b4d112ed865c19dceb28c97ef5d3543b5
KochyanLV <lvkochyan@sberbank.ru>
2025-11-16 20:47:32 +0300

    Update

--- sasrec/src/utils/__init__.py
+++ sasrec/src/utils/__init__.py
@@ -1,4 +1,3 @@
-from .arguments import get_args, DatasetArgs, ModelArgs, OptimizerArgs, TrainerArgs
 from .utils import (
     get_output_name,
     get_negative_samples,
@@ -9,11 +8,6 @@ from .get_scheduler import get_scheduler
 
 
 __all__ = [
-    "get_args",
-    "DatasetArgs",
-    "ModelArgs",
-    "OptimizerArgs",
-    "TrainerArgs",
     "get_output_name",
     "get_negative_samples",
     "get_positive2negatives",
